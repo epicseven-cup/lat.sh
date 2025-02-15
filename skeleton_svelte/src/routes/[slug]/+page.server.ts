0bca6c13e75937091a0136d7ef4b3bf966b51002
epicseven-cup <jackylin616@gmail.com>
2025-02-15 00:49:44 -0500

    added the query tags

--- skeleton_svelte/src/routes/[slug]/+page.server.ts
+++ skeleton_svelte/src/routes/[slug]/+page.server.ts
@@ -1,5 +1,13 @@
 import {redirect} from "@sveltejs/kit";
 
-export function load({ params }) {
-    redirect(301, `api/redirect/${params.slug}`)
+export function load({ params, url }) {
+	//console.log(params)
+	//console.log(url)
+	let o = url.searchParams('o')
+	let overview = url.searchParams('overview')
+	if (o != null or overview != null){
+		redirect(301, `api/redirect/${params.slug}?o=true`)
+	} else {
+		redirect(301, `api/redirect/${params.slug}`)
+	}
 }
