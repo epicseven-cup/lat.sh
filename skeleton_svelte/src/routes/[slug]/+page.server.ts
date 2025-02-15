f536c41fb4d411496a49b012bb04779c2dd65ff9
epicseven-cup <jackylin616@gmail.com>
2025-02-15 00:58:03 -0500

    get was removed during process by mistake

--- skeleton_svelte/src/routes/[slug]/+page.server.ts
+++ skeleton_svelte/src/routes/[slug]/+page.server.ts
@@ -3,8 +3,8 @@ import {redirect} from "@sveltejs/kit";
 export function load({ params, url }) {
 	//console.log(params)
 	//console.log(url)
-	let o = url.searchParams('o')
-	let overview = url.searchParams('overview')
+	let o = url.searchParams.get('o')
+	let overview = url.searchParams.get('overview')
 	if (o != null || overview != null){
 		redirect(301, `api/redirect/${params.slug}?o=true`)
 	} else {
